b13fcb9148e2f76fb7fcc270c3e91c58a8ae738d
Стефан Степанов <a1806430@yandex.ru>
2022-05-28 20:58:23 +0400

    Сделал алгоритм Беллмана-Форда

--- Lab_4.3/Graph.h
+++ Lab_4.3/Graph.h
@@ -1,5 +1,6 @@
-#include<list>
+#include <queue>
 #include<vector>
+#include <iostream>
 
 template<typename TVertex, typename TEdge>
 class Graph
@@ -19,29 +20,29 @@ private:
 	std::vector<Vertex> Table;
 	int Index(TVertex Temp_Vertex) // проверка, есть ли вершина с таким ID в графе
 	{
-		for (int i = 0; i < Table.size(); i++)
+		for (size_t i = 0; i < Table.size(); i++)
 		{
 			if (Table[i].First == Temp_Vertex) return Table[i].ID;
 		}
 		return -1;
 	}
 public:
-	
 	void Add_Vertex(TVertex Temp_Source) // добавить вершину
 	{
-		if (index(Temp_Source) < 0)
+		if (Index(Temp_Source) < 0)
 		{
 			Vertex New_Vertex;
 			New_Vertex.ID = Table.size();
-			New_Vertex.Source = Temp_Source;
+			New_Vertex.First = Temp_Source;
 			Table.push_back(New_Vertex);
+			
 		}
 		else throw "This vertex already exist";
 	}
 	void Add_Edge(TVertex Temp_Source, TVertex Temp_Dest, TEdge Temp_Edge) // добавить ребро
 	{
-		int Temp_Source_ID = index(Temp_Source);
-		int Temp_Dest_ID = index(Temp_Dest);
+		int Temp_Source_ID = Index(Temp_Source);
+		int Temp_Dest_ID = Index(Temp_Dest);
 		if (Temp_Source_ID < 0) throw "Source does not exist";
 		if (Temp_Dest_ID < 0) throw "Destination does not exist";
 		Destination New_Edge;
@@ -54,7 +55,7 @@ public:
 		int Temp_Source_ID = Index(Temp_Source);
 		int Temp_Dest_ID = Index(Temp_Dest);
 		if (Temp_Source_ID * Temp_Dest_ID < 0)  throw "Edge does not exist"; 
-		for (int i = 0; i < Table[Temp_Source_ID].Dest.size(); i++)
+		for (size_t i = 0; i < Table[Temp_Source_ID].Dest.size(); i++)
 		{
 			if (Table[Temp_Source_ID].Dest[i].ID == Temp_Dest_ID)
 			{
@@ -70,27 +71,14 @@ public:
 	void Delete_Vertex(TVertex Temp_Source) // удаление вершины
 	{
 		int index = Index(Temp_Source);
+		if (index < 0) throw "Vertex does not exist";
 		for (int i = 0; i < index; i++)
 		{
-			for (int j = 0; j < Table[i].Dest.size(); j++)
-			{
-				if (Table[i].Dest[j].Des == index)
-				{
-					for (int k = j; k < Table[i].Dest.size() - 1; k++)
-					{
-						Table[i].Dest[k].Dest = Table[i].Dest[k + 1].Dest;
-					}
-					Table[i].Dest.pop_back();
-				}
-			}
-		}
-		for (int i = index + 1; i < Table.size(); i++)
-		{
-			for (int j = 0; j < Table[i].Dest.size(); j++)
+			for (size_t j = 0; j < Table[i].Dest.size(); j++)
 			{
 				if (Table[i].Dest[j].Dest == index)
 				{
-					for (int k = j; k < Table[i].Dest.size() - 1; k++)
+					for (size_t k = j; k < Table[i].Dest.size() - 1; k++)
 					{
 						Table[i].Dest[k].Dest = Table[i].Dest[k + 1].Dest;
 					}
@@ -98,12 +86,75 @@ public:
 				}
 			}
 		}
-		for (int i = index; i < Table.size() - 1; i++)
+		for (size_t i = index + 1; i < Table.size(); i++)
+		{
+			for (size_t j = 0; j < Table[i].Dest.size(); j++)
+			{
+				if (Table[i].Dest[j].Dest == index)
+				{
+					for (size_t k = j; k < Table[i].Dest.size() - 1; k++)
+					{
+						Table[i].Dest[k].Dest = Table[i].Dest[k + 1].Dest;
+					}
+					Table[i].Dest.pop_back();
+				}
+			}
+		}
+		for (size_t i = index; i < Table.size() - 1; i++)
 		{
 			Table[i] = Table[i + 1];
 			Table[i].ID--;
 		}
 		Table.pop_back();
 	}
+	void Bellman_Ford(TVertex Temp_Source, TVertex Temp_Dest)
+	{
+		std::vector<double> d(Table.size());
+		std::fill(d.begin(),d.end(), INFINITY);
+		d[Index(Temp_Source)] = 0;
+		std::vector<std::vector<TVertex>> Ways(Table.size());
+		for (size_t i = 0; i < Table.size(); i++)
+		{
+			for (size_t j = 0; j < Table[i].Dest.size(); j++)
+			{
+				if (d[i] + Table[i].Dest[j].Edge < d[Table[i].Dest[j].Dest])
+				{
+					d[Table[i].Dest[j].Dest] = d[i] + Table[i].Dest[j].Edge;
+					Ways[Table[i].Dest[j].Dest] = Ways[i];
+					Ways[Table[i].Dest[j].Dest].push_back(Table[i].ID + 1);
+				}
+			}
+		}
+		for (size_t i = 0; i < Table.size(); i++)
+		{
+			Ways[i].push_back(Table[i].ID + 1);
+		}
+		for (size_t i = 0; i < Table.size(); i++)
+		{
+			for (size_t j = 0; j < Ways[i].size(); j++)
+			{
+				std::cout << Ways[i][j];
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+		for (int i = 0; i < Index(Temp_Source); i++)
+		{
+			if (d[i] == INFINITY)
+			{
+				std::cout << std::endl << Temp_Source << "->" << i + 1 << "=" << "No way";
+			}
+			else std::cout << std::endl << Temp_Source << "->" << i + 1 << "=" << d[i];
+		}
+		for (size_t i = Index(Temp_Source) + 1; i < Table.size(); i++)
+		{
+			if (d[i] == INFINITY)
+			{
+				std::cout << std::endl << Temp_Source << "->" << i + 1 << "=" << "No way";
+			}
+			else std::cout << std::endl << Temp_Source << "->" << i + 1 << "=" << d[i];
+		}
+	}
+	
 };
 
--- Lab_4.3/Lab_4.3.cpp
+++ Lab_4.3/Lab_4.3.cpp
@@ -1,9 +1,22 @@
-﻿
+﻿#include "Graph.h"
 #include <iostream>
 
 int main()
 {
-    
+    Graph<int, int> Town;
+    Town.Add_Vertex(1);
+    Town.Add_Vertex(2);
+    Town.Add_Vertex(3);
+    Town.Add_Vertex(4);
+    Town.Add_Vertex(5);
+    Town.Add_Edge(4, 1, 2);
+    Town.Add_Edge(1, 4, 3);
+    Town.Add_Edge(3, 4, 5);
+    Town.Add_Edge(1, 2, 4);
+    Town.Add_Edge(4, 3, 6);
+    Town.Add_Edge(3, 2, 7);
+    Town.Bellman_Ford(3, 4);
+    return 0;
 }
 
 
